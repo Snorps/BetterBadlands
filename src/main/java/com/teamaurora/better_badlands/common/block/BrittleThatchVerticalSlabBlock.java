7a93bd28988649787013e052c92028066a669586
Epic312 <kaden_ostendorf@yahoo.com>
2020-09-05 12:22:46 -0600

    dead bush thatch breaks on contact w/ lava

---
+++ src/main/java/com/teamaurora/better_badlands/common/block/BrittleThatchVerticalSlabBlock.java
@@ -0,0 +1,45 @@
+package com.teamaurora.better_badlands.common.block;
+
+import com.teamabnormals.abnormals_core.common.blocks.thatch.ThatchVerticalSlabBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
+
+import java.util.Random;
+
+@SuppressWarnings("deprecated")
+public class BrittleThatchVerticalSlabBlock extends ThatchVerticalSlabBlock {
+    public BrittleThatchVerticalSlabBlock (Properties properties) {
+        super(properties);
+    }
+
+    public void onBlockAdded(BlockState state, World worldIn, BlockPos pos, BlockState oldState, boolean isMoving) {
+        boolean flag = false;
+        for (Direction dir : Direction.values()) {
+            if (worldIn.getBlockState(pos.offset(dir)).getBlock() == Blocks.LAVA) {
+                flag = true;
+            }
+        }
+        if (flag) {
+            worldIn.destroyBlock(pos, false);
+        }
+        super.onBlockAdded(state, worldIn, pos, oldState, isMoving);
+    }
+
+    public void neighborChanged(BlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos fromPos, boolean isMoving) {
+        boolean flag = false;
+        for (Direction dir : Direction.values()) {
+            if (worldIn.getBlockState(pos.offset(dir)).getBlock() == Blocks.LAVA) {
+                flag = true;
+            }
+        }
+        if (flag) {
+            worldIn.destroyBlock(pos, false);
+        }
+        super.neighborChanged(state, worldIn, pos, blockIn, fromPos, isMoving);
+    }
+}
--- src/main/java/com/teamaurora/better_badlands/core/registry/BetterBadlandsBlocks.java
+++ src/main/java/com/teamaurora/better_badlands/core/registry/BetterBadlandsBlocks.java
@@ -5,6 +5,10 @@ import com.teamabnormals.abnormals_core.common.blocks.thatch.ThatchSlabBlock;
 import com.teamabnormals.abnormals_core.common.blocks.thatch.ThatchStairsBlock;
 import com.teamabnormals.abnormals_core.common.blocks.thatch.ThatchVerticalSlabBlock;
 import com.teamabnormals.abnormals_core.core.utils.RegistryHelper;
+import com.teamaurora.better_badlands.common.block.BrittleThatchBlock;
+import com.teamaurora.better_badlands.common.block.BrittleThatchSlabBlock;
+import com.teamaurora.better_badlands.common.block.BrittleThatchStairsBlock;
+import com.teamaurora.better_badlands.common.block.BrittleThatchVerticalSlabBlock;
 import com.teamaurora.better_badlands.core.BetterBadlands;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
@@ -19,8 +23,8 @@ import net.minecraftforge.fml.common.Mod;
 public class BetterBadlandsBlocks {
     public static final RegistryHelper HELPER = BetterBadlands.REGISTRY_HELPER;
 
-    public static final RegistryObject<Block> DEAD_BUSH_THATCH = HELPER.createBlock("dead_bush_thatch", () -> new ThatchBlock(AbstractBlock.Properties.from(Blocks.HAY_BLOCK).notSolid().harvestTool(ToolType.AXE)), ItemGroup.BUILDING_BLOCKS);
-    public static final RegistryObject<Block> DEAD_BUSH_THATCH_SLAB = HELPER.createBlock("dead_bush_thatch_slab", () -> new ThatchSlabBlock(AbstractBlock.Properties.from(Blocks.HAY_BLOCK).notSolid().harvestTool(ToolType.AXE)), ItemGroup.BUILDING_BLOCKS);
-    public static final RegistryObject<Block> DEAD_BUSH_THATCH_STAIRS = HELPER.createBlock("dead_bush_thatch_stairs", () -> new ThatchStairsBlock(DEAD_BUSH_THATCH.get().getDefaultState(), AbstractBlock.Properties.from(Blocks.HAY_BLOCK).notSolid().harvestTool(ToolType.AXE)), ItemGroup.BUILDING_BLOCKS);
-    public static final RegistryObject<Block> DEAD_BUSH_THATCH_VERTICAL_SLAB = HELPER.createCompatBlock("quark", "dead_bush_thatch_vertical_slab", () -> new ThatchVerticalSlabBlock(AbstractBlock.Properties.from(DEAD_BUSH_THATCH.get()).notSolid()), ItemGroup.BUILDING_BLOCKS);
+    public static final RegistryObject<Block> DEAD_BUSH_THATCH = HELPER.createBlock("dead_bush_thatch", () -> new BrittleThatchBlock(AbstractBlock.Properties.from(Blocks.HAY_BLOCK).notSolid().harvestTool(ToolType.AXE)), ItemGroup.BUILDING_BLOCKS);
+    public static final RegistryObject<Block> DEAD_BUSH_THATCH_SLAB = HELPER.createBlock("dead_bush_thatch_slab", () -> new BrittleThatchSlabBlock(AbstractBlock.Properties.from(Blocks.HAY_BLOCK).notSolid().harvestTool(ToolType.AXE)), ItemGroup.BUILDING_BLOCKS);
+    public static final RegistryObject<Block> DEAD_BUSH_THATCH_STAIRS = HELPER.createBlock("dead_bush_thatch_stairs", () -> new BrittleThatchStairsBlock(DEAD_BUSH_THATCH.get().getDefaultState(), AbstractBlock.Properties.from(Blocks.HAY_BLOCK).notSolid().harvestTool(ToolType.AXE)), ItemGroup.BUILDING_BLOCKS);
+    public static final RegistryObject<Block> DEAD_BUSH_THATCH_VERTICAL_SLAB = HELPER.createCompatBlock("quark", "dead_bush_thatch_vertical_slab", () -> new BrittleThatchVerticalSlabBlock(AbstractBlock.Properties.from(DEAD_BUSH_THATCH.get()).notSolid()), ItemGroup.BUILDING_BLOCKS);
 }
