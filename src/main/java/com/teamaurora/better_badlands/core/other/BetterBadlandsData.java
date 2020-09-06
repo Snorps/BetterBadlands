7ba8b3a33ac70dbe9fa40c62ef75aaf1d997c02c
Epic312 <kaden_ostendorf@yahoo.com>
2020-09-06 16:46:45 -0600

    upped thatch burn speed

--- src/main/java/com/teamaurora/better_badlands/core/other/BetterBadlandsData.java
+++ src/main/java/com/teamaurora/better_badlands/core/other/BetterBadlandsData.java
@@ -5,9 +5,9 @@ import com.teamaurora.better_badlands.core.registry.BetterBadlandsBlocks;
 
 public class BetterBadlandsData {
     public static void registerFlammables() {
-        DataUtils.registerFlammable(BetterBadlandsBlocks.DEAD_BUSH_THATCH.get(), 60, 60);
-        DataUtils.registerFlammable(BetterBadlandsBlocks.DEAD_BUSH_THATCH_SLAB.get(), 60, 60);
-        DataUtils.registerFlammable(BetterBadlandsBlocks.DEAD_BUSH_THATCH_STAIRS.get(), 60, 60);
-        DataUtils.registerFlammable(BetterBadlandsBlocks.DEAD_BUSH_THATCH_VERTICAL_SLAB.get(), 60, 60);
+        DataUtils.registerFlammable(BetterBadlandsBlocks.DEAD_BUSH_THATCH.get(), 150, 150);
+        DataUtils.registerFlammable(BetterBadlandsBlocks.DEAD_BUSH_THATCH_SLAB.get(), 150, 150);
+        DataUtils.registerFlammable(BetterBadlandsBlocks.DEAD_BUSH_THATCH_STAIRS.get(), 150, 150);
+        DataUtils.registerFlammable(BetterBadlandsBlocks.DEAD_BUSH_THATCH_VERTICAL_SLAB.get(), 150, 150);
     }
 }
